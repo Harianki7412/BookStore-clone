fd97edd7522fbf8f5cbc8bbd2d2b2387366e9858
Hariom verma <hariom.verma00004@gmail.com>
2024-12-28 21:55:33 -0800

    backend is redey

--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -47,7 +47,7 @@ function Freebook() {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get('http://localhost:4001/book');
+        const res = await axios.get('https://bookstore-ieql.onrender.com/book');
         console.log(res.data);
         setBook(res.data.filter((data) => data.category === 'Free'));
       } catch (error) {
